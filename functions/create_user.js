259bcc4c522b63ff4ccab9bebdb59153e30bb5c6
sjaymoon15 <sjaymoon15@gmail.com>
2017-09-04 14:25:48 -0700

    set up twilio

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,18 @@
+const admin = require('firebase-admin');
+//+16314964077
 module.exports = function(req, res) {
-  res.send(req.body);
+  //verify the user provided a phone number
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+
+  //format the phone number to remove dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  // create a new user account using the phone number
+  admin.auth().createUser({ uid: phone })
+  //respond to the user request, saying the account was made
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
+
 }
